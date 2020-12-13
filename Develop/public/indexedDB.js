99c84f3869b1f669e7827124596bb35776a06864
Eric Vondunn <ericvondunn40@gmail.com>
2020-12-13 14:27:45 -0500

    additional updates to heroku and mongo

--- Develop/public/indexedDB.js
+++ Develop/public/indexedDB.js
@@ -5,14 +5,23 @@ const indexedDB =
   window.mozIndexedDB ||
   window.webkitIndexedDB ||
   window.msIndexedDB ||
-  window.shimIndexedDB ||
+  window.shimIndexedDB;
 
 let db;
+
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://evondunn:<Milk40%21%2117>@cluster0.gkaou.mongodb.net/<transactions_db>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 //budget is the name of our cache
 //opens budget
 const request = indexedDB.open("budget", 1);
-
 
 // this is the schema
 request.onupgradeneeded = ({ target }) => {
@@ -23,29 +32,30 @@ request.onupgradeneeded = ({ target }) => {
 request.onsuccess = ({ target }) => {
   db = target.result;
 
-
-// check if app is online before it actually reads the info form the db
-if (navigator.onLine) {
-  checkDatabase();
-}
+  // check if app is online before it actually reads the info form the db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
 };
 
-request.onerror = function(e) {
+request.onerror = function (e) {
   console.log("Something went wrong " + e.target.errorCode);
 };
 
+//save record to db if offline
 function saveRecord(record) {
   const transaction = db.transaction(["pending"], "readwrite");
   const store = transaction.objectStore("pending");
 }
 
+// back online send to mongo and clear pending index
 function checkDatabase() {
   // getting the reference to the db
   const transaction = db.transaction(["pending"], "readwrite");
   const store = transaction.objectStore("pending");
   const getAll = store.getAll();
 
-  getAll.onsuccess = function() {
+  getAll.onsuccess = function () {
     // posts if you have something bulk
     if (getAll.result.length > 0) {
       fetch("/api/transaction/bulk", {
@@ -53,49 +63,36 @@ function checkDatabase() {
         body: JSON.stringify(getAll.result),
         headers: {
           Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
+          "Content-Type": "application/json",
+        },
       })
-      .then(response => {
-        return response.json();
-      })
-      .then(() => {
-        // delete records if successful
-        const transaction = db.transaction(["pending"], "readwrite");
-        const store = transaction.objectStore("pending");
-        store.clear();
-      });
+        .then((response) => {
+          return response.json();
+        })
+        .then(() => {
+          // delete records if successful
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
     }
   };
-}
-
-
-
-
 }
 
 // listening for app to come back online
 window.addEventListener("online", checkDatabase);
 
+// working on this
+function saveRecord(data) {
+  console.log(data);
 
+  // create budget db + bulk (pending) collection
 
+  // install -> create bulk (pending) collection
 
+  // activate ->
 
+  // saveRecord -> save to indexedDB
 
-  // working on this 
-  function saveRecord(data) {
-    console.log(data);
-
-
-
-
-    // create budget db + bulk (pending) collection
-
-    // install -> create bulk (pending) collection
-
-    // activate ->
-
-    // saveRecord -> save to indexedDB
-
-    // listen to when we are back online and send records
-  };
+  // listen to when we are back online and send records
+}
--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,23 +1,45 @@
-var CACHE_NAME = "my-site-cache-v1";
+const CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-var urlsToCache = [
+var FILES_TO_CACHE = [
   "/",
   "/db.js",
-  "index.js,"
-  "/manifest.json",
+  "index.js",
+  ".manifest.json",
   "/styles.css",
   "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
-
+  "/icons/icon-512x512.png",
 ];
 
-self.addEventListener("install", function(event){
+self.addEventListener("install", function (e) {
   //perform install steps
 
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache){
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
       console.log("opened cache");
     })
-    );
-})
+  );
+
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  //browser activate SW once it installs
+  self.skipWaiting();
+});
+
+// actual activation
+self.addEventListener("activate", function (e) {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
